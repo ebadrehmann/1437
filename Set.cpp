4a9c5abf328a172e2f797380b02fe57f649ba987
Ebad Rehman <106476276+ebadrehmann@users.noreply.github.com>
2026-09-04 22:41:07 -0500

    Delete Set.cpp

--- Set.cpp
+++
@@ -1,9 +0,0 @@
-#include "Set.h"
-#include <iostream>
-using namespace std;
-
-void Set::display() {
-    cout << "    Reps:     " << reps << endl;
-    cout << "    Weight:   " << weight << " lbs" << endl;
-    cout << "    Duration: " << duration << " sec" << endl;
-}
